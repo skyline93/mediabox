d8c0eaf69cdfc0fad5ac762df2edb250a3939482
skyline93 <glf9832@163.com>
2024-10-21 21:05:13 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.21
+FROM golang:1.22
 
 ARG SOURCE_MIRROR
 
--- internal/mediabox/worker.go
+++ internal/mediabox/worker.go
@@ -2,195 +2,160 @@ package mediabox
 
 import (
 	"context"
-	"log"
 	"sync"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
 
-var Pool *WorkerPool
-
-func InitWorkPool() {
-	ctx := context.TODO()
-	Pool = NewPool(ctx, 1)
-}
-
 type Job interface {
-	Run()
+	Run() error
 }
 
 type WorkerPool struct {
 	JobChan     chan Job
 	Concurrency int
 
-	Workers map[string]Worker
+	Workers map[string]worker
 	mut     sync.RWMutex
 
 	addWorkerChan    chan struct{}
 	cancelWorkerChan chan struct{}
 
-	ctx context.Context
-
-	cancel   context.CancelFunc
-	stopChan chan struct{}
-	wg       sync.WaitGroup // 用于等待所有 worker 完成
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
-func NewPool(ctx context.Context, concurrent int) *WorkerPool {
+func NewWorkerPool(ctx context.Context, concurrent int) *WorkerPool {
 	ctx, cancel := context.WithCancel(ctx)
 
-	p := &WorkerPool{
+	return &WorkerPool{
 		JobChan:          make(chan Job),
 		addWorkerChan:    make(chan struct{}),
 		cancelWorkerChan: make(chan struct{}),
 		Concurrency:      concurrent,
-		Workers:          make(map[string]Worker),
+		Workers:          make(map[string]worker),
 		ctx:              ctx,
 		cancel:           cancel,
-		stopChan:         make(chan struct{}),
+	}
+}
+
+func (p *WorkerPool) Run() {
+	for i := 0; i < p.Concurrency; i++ {
+		p.addWorker()
 	}
 
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+	for {
+		select {
+		case <-p.addWorkerChan:
+			p.addWorker()
 
-	go func() {
-		for {
-			select {
-			case <-p.addWorkerChan:
-				p.wg.Add(1) // 增加等待组计数器
+		case <-p.cancelWorkerChan:
+			p.delOnceWorker()
 
-				w := New(p.ctx, p.JobChan, &p.wg)
-				go w.Run()
-				log.Printf("new worker [%s]", w.ID)
-
-				p.mut.Lock()
-				p.Workers[w.ID] = *w
-				log.Printf("add worker [%s] to pool", w.ID)
-				p.mut.Unlock()
-
-			case <-p.cancelWorkerChan:
-				var worker Worker
-				p.mut.RLock()
-				for _, w := range p.Workers {
-					worker = w
-					break
-				}
-				p.mut.RUnlock()
-
-				log.Printf("cancel worker [%s]", worker.ID)
-				worker.Cancel()
-
-				p.mut.Lock()
-				delete(p.Workers, worker.ID)
-				log.Printf("remove worker [%s] from pool", worker.ID)
-				p.mut.Unlock()
-
-			case <-ticker.C:
-				if len(p.Workers) < p.Concurrency {
-					go func() {
-						p.addWorkerChan <- struct{}{}
-					}()
-				}
-
-				if len(p.Workers) > p.Concurrency {
-					go func() {
-						p.cancelWorkerChan <- struct{}{}
-					}()
-				}
-
-			case <-p.stopChan:
-				log.Println("stopping worker pool")
-				p.cancel()
-				p.mut.Lock()
-				for _, w := range p.Workers {
-					w.Cancel()
-				}
-				p.mut.Unlock()
-				close(p.JobChan)
-				return
+		case <-time.NewTicker(time.Second * 1).C:
+			if len(p.Workers) < p.Concurrency {
+				go func() {
+					p.addWorkerChan <- struct{}{}
+				}()
 			}
-		}
-	}()
 
-	return p
+			if len(p.Workers) > p.Concurrency {
+				go func() {
+					p.cancelWorkerChan <- struct{}{}
+				}()
+			}
+		case <-p.ctx.Done():
+			return
+		}
+	}
+}
+
+func (p *WorkerPool) addWorker() {
+	w := newWorker(p.ctx, p.JobChan)
+	go w.Run()
+	logger.Infof("new worker [%s]", w.ID)
+
+	p.mut.Lock()
+	p.Workers[w.ID] = *w
+	p.mut.Unlock()
+
+	logger.Infof("add worker [%s] to pool", w.ID)
+}
+
+func (p *WorkerPool) delOnceWorker() {
+	var worker worker
+
+	p.mut.RLock()
+	for _, w := range p.Workers {
+		worker = w
+		break
+	}
+	p.mut.RUnlock()
+
+	logger.Infof("cancel worker [%s]", worker.ID)
+	worker.Cancel()
+
+	p.mut.Lock()
+	delete(p.Workers, worker.ID)
+	p.mut.Unlock()
+
+	logger.Infof("delete worker [%s] from pool", worker.ID)
 }
 
 func (p *WorkerPool) Submit(j Job) {
 	p.JobChan <- j
 }
 
-func (p *WorkerPool) SetPoolSize(c int) {
+func (p *WorkerPool) Scale(poolSize int) {
 	p.Concurrency = c
 }
 
-func (p *WorkerPool) GetCurrentWorkers() (ws []*Worker) {
-	p.mut.RLock()
-	defer p.mut.RUnlock()
-
-	for _, w := range p.Workers {
-		ws = append(ws, &w)
-	}
-
-	return
-}
-
-type Worker struct {
+type worker struct {
 	ID      string
 	jobChan chan Job
 
 	ctx    context.Context
 	Cancel context.CancelFunc
-
-	wg *sync.WaitGroup
 }
 
-func New(ctx context.Context, jobChan chan Job, wg *sync.WaitGroup) *Worker {
+func newWorker(ctx context.Context, jobChan chan Job) *worker {
 	c, cancel := context.WithCancel(ctx)
 
-	return &Worker{
+	return &worker{
 		ID:      uuid.NewV4().String(),
 		jobChan: jobChan,
 
 		ctx:    c,
 		Cancel: cancel,
-		wg:     wg,
 	}
 }
 
-func (w *Worker) Run() {
-	defer w.wg.Done()
-
+func (w *worker) Run() {
 	for {
 		select {
 		case job := <-w.jobChan:
-			log.Printf("worker [%s] receive job", w.ID)
+			logger.Infof("worker [%s] receive job", w.ID)
 			w.run(job)
 		case <-w.ctx.Done():
-			log.Printf("worker [%s] exit", w.ID)
+			logger.Infof("worker [%s] exit", w.ID)
 			return
 		}
 	}
 }
 
-func (w *Worker) run(j Job) {
+func (w *worker) run(j Job) {
+	var err error
+
 	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("job error, msg: %v", err)
+		if err != nil {
+			logger.Errorf("job error, msg: %v", err)
 		}
 	}()
 
-	j.Run()
+	err = j.Run()
 }
 
-func (w *Worker) Submit(j Job) {
+func (w *worker) Submit(j Job) {
 	w.jobChan <- j
-}
-
-func (p *WorkerPool) Stop() {
-	close(p.stopChan)
-}
-
-func (p *WorkerPool) Wait() {
-	p.wg.Wait()
 }
